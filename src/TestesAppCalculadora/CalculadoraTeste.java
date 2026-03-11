2827fb6932f5b2f36217d5e7d239d643a618f2eb
Vanessa Fittipaldi <88788638+vxnesv@users.noreply.github.com>
2026-03-10 22:06:55 -0300

    Update CalculadoraTeste.java

--- src/TestesAppCalculadora/CalculadoraTeste.java
+++ src/TestesAppCalculadora/CalculadoraTeste.java
@@ -2,87 +2,172 @@ package TestesAppCalculadora;
 
 import static org.junit.Assert.*;
 
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import FontesAppCalculadora.CalculadoraAritmetica;
+import FontesAppCalculadora.Calculadora;
 
-
-
-public class CalculadoraTeste {
+public class CalculadoraAritmeticaTest {
 	
-		//atributos de controle do teste
-		//cada teste executado é um objeto novo
+	//Atributos de controle do teste
 		private static int numeroTeste = 0;
 		private String nomeTeste = "";
 		private double resultadoReal = 0;
 		
-		//Atributos de comparação de resultado
-		private double resultadoEsperado;	
-		
-		//preparação do teste
+		//PREPARAÇÃO DO TESTE
 		@Before
-		
 		public void preparaTeste() {
-			numeroTeste += 1;
-			System.out.print("Iniciando o teste: ");
+			numeroTeste = numeroTeste + 1;
+			System.out.print("Iniciando o teste número: ");
 			System.out.println(numeroTeste);
 		}
 		
+		//ENCERRAMENTO DO TESTE
 		@After
 		public void encerraTeste() {
-			System.out.println(nomeTeste);
-			System.out.print(" - resultado = ");
-			System.out.println(resultadoReal);
+		    System.out.print(nomeTeste);
+		    System.out.print(" - resultado = ");
+		    System.out.println(resultadoReal);
 		}
 		
-		@Test
-		public void testeSomar2com3() {
-			nomeTeste = "Teste de soma de 2 com 3";
-			resultadoEsperado = 5;
-			CalculadoraAritmetica calc = new CalculadoraAritmetica();
-			resultadoReal = calc.somar(2, 3);
-			assertEquals(resultadoEsperado, resultadoReal, 0);
-		}
 		
-		@Test
-		public void testeSomar3com3() {
-			nomeTeste = "Teste de soma de 3 com 3";
-			resultadoEsperado = 6;
-			CalculadoraAritmetica calc = new CalculadoraAritmetica();
-			resultadoReal = calc.somar(3, 3);
-			assertEquals(resultadoEsperado, resultadoReal, 0);
-		}
-		
-		@Test
-		public void testeSomar5com3() {
-			nomeTeste = "Teste de soma de 5 com 3";
-			resultadoEsperado = 8;
-			CalculadoraAritmetica calc = new CalculadoraAritmetica();
-			resultadoReal = calc.somar(5, 3);
-			assertEquals(resultadoEsperado, resultadoReal, 0);
-		}
-		
-			@Test
-		public void testeDividir3com2() {
-			nomeTeste = "Teste de divisão de 3 com 2";
-			resultadoEsperado = 0.4285;
-			CalculadoraAritmetica calc = new CalculadoraAritmetica();
-			resultadoReal = calc.dividir(3, 7);
-			assertEquals(resultadoEsperado, resultadoReal, 0.1);
-		}
-			
-		@Test
-		public void testeDividir3com7() {
-			nomeTeste = "Teste de divisão de 3 com 7";
-			resultadoEsperado = 0.42857;
-			CalculadoraAritmetica calc = new CalculadoraAritmetica();
-			resultadoReal = calc.dividir(3, 7);
-			//esses 0.0001 na mascara deve igualar ao que foi colocado no resultadoEsperado
-			assertEquals(resultadoEsperado, resultadoReal, 0.0001);
-		}
-  
+	    /** 
+	    * Teste de somar na Calculadora. 
+
+	    */ 
+
+	   @Test 
+
+	   public void testeSomar5com5() { 
+		   nomeTeste = "Teste de soma de 5 com 5";
+		        
+
+		   double nro1 = 5; 
+
+		   double nro2 = 5; 
+
+	       Calculadora calc= new Calculadora(); 
+
+	       double resultadoEsperado = 10; 
+
+	       resultadoReal= calc.somar(nro1, nro2);       
+	      
+	       assertEquals(resultadoEsperado, resultadoReal, 0); 
+
+	   } 
+
+	 
+
+	   /** 
+
+	   * Teste de subtrair na  Calculadora. 
+
+	   */ 
+
+	  @Test 
+
+	  public void testeSubtrair3de5() {
+		   nomeTeste = "Teste de subtrair 3 de 5";
+
+
+		   double nro1 = 5; 
+
+		   double nro2 = 3; 
+
+	      Calculadora calc = new Calculadora(); 
+
+	      double resultadoEsperado= 2; 
+
+	      resultadoReal= calc.subtrair(nro1, nro2); 
+
+	     
+	      assertEquals(resultadoEsperado, resultadoReal, 0);   
+
+	  } 
+
+	   
+
+	   /** 
+
+	   * Teste de multiplicar na Calculadora. 
+
+	   */ 
+
+	   @Test 
+
+	   public void testeMultiplicar3por3() {
+		   nomeTeste = "Teste de multiplicar 3 por 3";
+
+		   
+		   double nro1 = 3; 
+
+		   double nro2 = 3; 
+
+	       Calculadora calc = new Calculadora(); 
+
+	       double resultadoEsperado = 9; 
+
+	       resultadoReal = calc.multiplicar(nro1, nro2); 
+
+	       
+	       assertEquals(resultadoEsperado, resultadoReal, 0); 
+
+	   } 
+
+	 
+
+	   /** 
+
+	   * Teste de dividir na Calculadora. 
+
+	   */ 
+
+	  @Test 
+
+	  public void testeDividir3por2() { 
+		   nomeTeste = "Teste de dividir 3 por 2";
+
+		   
+		   double nro1 = 3; 
+
+		   double nro2 = 2; 
+
+	      Calculadora calc = new Calculadora(); 
+
+	      double resultadoEsperado= 1.5; 
+
+	      resultadoReal = calc.dividir(nro1, nro2); 
+
+
+	      
+	      assertEquals(resultadoEsperado, resultadoReal, 0.1); 
+
+	  } 
+
+	   
+
+	  @Test 
+
+	  public void testeDividir3por7() { 
+		   nomeTeste = "Teste de dividir 3 por 7";
+
+		   
+		   double nro1 = 3; 
+
+		   double nro2 = 7; 
+
+	      Calculadora calc = new Calculadora(); 
+
+	      double resultadoEsperado= 0.4285; 
+
+	      resultadoReal = calc.dividir(nro1, nro2); 
+
+
+	      
+	      assertEquals(resultadoEsperado, resultadoReal, 0.0001); 
+
+	  } 
 }
+
 
