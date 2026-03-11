64cc4fe8fa9d9161dda82a13af9caf56184cab81
Vanessa Fittipaldi <88788638+vxnesv@users.noreply.github.com>
2026-03-10 22:06:11 -0300

    Update CalculadoraAritmetica.java

--- src/FontesAppCalculadora/CalculadoraAritmetica.java
+++ src/FontesAppCalculadora/CalculadoraAritmetica.java
@@ -1,57 +1,68 @@
 package FontesAppCalculadora;
 
-import java.util.Scanner;
+public class Calculadora{ 
 
-public class CalculadoraAritmetica {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		double valor1, valor2;
-		double opcao = 0;
-		
-		System.out.println("Valor1: ");
-		valor1 = sc.nextInt();
-		
-		System.out.println("Valor2: ");
-		valor2 = sc.nextInt();
-		
-		System.out.println("Digite sua opção: \n1. Somar\n2. Subtrair\n3. Multiplicar\n4. Dividir");
-		
-		if (opcao == 1) {
-			System.out.println(somar(valor1, valor2)); 
-		}else if (opcao == 2) {
-			System.out.println(subtrair(valor1, valor2));
-		}else if (opcao == 3) {
-			System.out.println(multiplicar(valor1, valor2));
-		}
-		else {
-			System.out.println(dividir(valor1, valor2));
-		}
-		
-	}
-	
-	private int resultadoConta = 0;
-	
-	public static double somar(double valor1, double valor2) {
-		double resultadoConta = valor1 + valor2;
-		return resultadoConta;
-	}
-	
-	public static double subtrair(double valor1, double valor2) {
-		double resultadoConta = valor1 - valor2;
-		return resultadoConta;
-	}
-	
-	public static double multiplicar(double valor1, double valor2) {
-		double resultadoConta = valor1 * valor2;
-		return resultadoConta;
-	}
-	
-	public static double dividir(double valor1, double valor2) {
-		double resultadoConta = valor1 / valor2;
-		return resultadoConta;
-	}
-	
 
-}
+    // atributo 
+
+    private double resultado = 0; 
+
+
+
+    // método somar 
+
+    public double somar( double n1, double n2 ){ 
+
+
+
+        resultado = n1 + n2; 
+
+        return resultado; 
+
+    } 
+
+
+
+    // método subtrair 
+
+    public double subtrair(double n1, double n2 ){ 
+
+
+
+        resultado = n1 - n2; 
+
+        return resultado; 
+
+    }         
+
+
+
+    // método multiplicar 
+
+    public double multiplicar( double n1, double n2 ){ 
+
+
+
+        resultado = n1 * n2; 
+
+        return resultado; 
+
+    } 
+
+
+
+    // método dividir 
+
+    public double dividir( double n1, double n2 ){ 
+
+
+
+        resultado = n1 / n2; 
+
+        return resultado; 
+
+    } 
+
+} 
+
